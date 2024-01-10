5bfd5fb34b585b1be079fb8008985f1255a99d44
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-01-10 22:49:05 +0530

    Time: 12 ms (88.94%), Space: 79.5 MB (53.66%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+class Solution {
+        class Pair{
+        boolean f;
+        int x;
+        Pair(boolean f,int x){
+            this.f=f;
+            this.x=x;
+        }
+    }
+    int max;
+    public int amountOfTime(TreeNode root, int start) {
+        max=0;
+        sol(root,start);
+        return max;
+    }
+    public Pair sol(TreeNode root,int start){
+        if(root==null) return new Pair(false,0);
+        Pair left=sol(root.left,start);
+        Pair right=sol(root.right,start);
+        if(root.val==start){
+            max=Math.max(max,Math.max(left.x,right.x));
+            return new Pair(true,1);
+        }
+        if(left.f){
+            max=Math.max(max,left.x+right.x);
+            left.x+=1;
+            return left;
+        }else if(right.f){
+            max=Math.max(max,left.x+right.x);
+            right.x+=1;
+            return right;
+        }
+        return new Pair(false,Math.max(left.x,right.x)+1);
+    }
+}
