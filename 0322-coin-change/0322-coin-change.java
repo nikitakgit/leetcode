685411f309a7f02fa73d4b65a3f22879f1a6bb2f
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-01-12 13:36:03 +0530

    Time: 37 ms (18.32%), Space: 45.1 MB (14.12%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[][] dp=new int[coins.length][amount+1];
+        for(int[] row:dp)
+        {
+            Arrays.fill(row,-1);
+        }
+        int ans= f(coins.length-1,coins,amount,dp);
+        if(ans>=(int)Math.pow(10,9))
+        {
+            return -1;
+        }
+        return ans;
+    }
+    
+    public int f(int i,int[] coins,int target,int[][] dp)
+    {   
+        
+        if(i==0)
+        {
+            if(target%coins[i]==0)
+            {
+                return target/coins[0];
+            }
+            else return (int)Math.pow(10,9);
+        }
+        if(dp[i][target]!=-1)
+        {
+            return dp[i][target];
+        }
+        int notTake=0+f(i-1,coins,target,dp);
+        int Take=Integer.MAX_VALUE;
+        if(coins[i]<=target)
+        {
+            Take=1+f(i,coins,target-coins[i],dp);
+        }
+        
+         dp[i][target]=Math.min(Take,notTake);
+        return dp[i][target];
+        
+    }
+    
+    
+}
