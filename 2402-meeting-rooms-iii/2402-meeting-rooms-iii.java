924c732ac7d21dbef70fa88152b7bd2ae1cf15b8
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-02-18 16:44:33 +0530

    Time: 50 ms (99.80%), Space: 100.9 MB (23.46%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+        int[] ans=new int[n];
+        long[] times=new long[n];
+        Arrays.sort(meetings,(a,b) -> Integer.compare(a[0],b[0]));
+        
+        for(int i=0;i<meetings.length;i++)
+        {
+            int start=meetings[i][0];
+            int end=meetings[i][1];
+            
+            boolean flag=false;
+            int minIn=-1;
+            long val=Long.MAX_VALUE;
+            
+            for(int j=0;j<n;j++)
+            {
+                if(times[j]<val)
+                {
+                    val=times[j];
+                    minIn=j;
+                }
+                if(times[j]<=start)
+                {
+                    flag=true;
+                    ans[j]++;
+                    times[j]=end;
+                    break;
+                }
+            }
+            if(!flag)
+            {
+                ans[minIn]++;
+                times[minIn]+=(end-start);
+            }
+        }
+        int maxi=-1,id=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(ans[i]>maxi)
+            {
+                maxi=ans[i];
+                id=i;
+            }
+        }
+        return id;
+    }
+}
