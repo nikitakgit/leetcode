831ce5eedfba007030aa7cf59a435f52406a9387
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-03-15 21:52:52 +0530

    Time: 2 ms (73.94%), Space: 53.4 MB (49.63%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,10 +3,13 @@ class Solution {
        int[] ans=new int[nums.length];
         int n=nums.length;
         ans[0]=1;
+        //calculate the left product and store it in ans array
         for(int i=1;i<n;i++)
         {
             ans[i]=ans[i-1]*nums[i-1];
         }
+        
+        //calculate the right product and update the ans array
         int rightProduct=1;
         for(int i=n-1;i>=0;i--)
         {
