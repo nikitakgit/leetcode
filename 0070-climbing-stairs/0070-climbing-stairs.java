08f8acbfee738e5b2aaf7098c176a9e0f081241e
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-05-27 23:27:37 +0530

    Time: 0 ms (100.00%), Space: 40 MB (72.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==1) return 1;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+    public int solve(int i,int[] dp)
+    {
+        if(i<0) return 0;
+        if(i==0) return 1;
         
-        if(n==2) return 2;
-
-        int[] a =  new int[n];
-        a[0]=1;
-        a[1]=2;
-
-        for(int i=2;i<n;i++){
-            a[i]=a[i-1]+a[i-2];
-        }
-        return a[n-1];
+        if(dp[i]!=-1) return dp[i];
+        dp[i]=solve(i-1,dp)+solve(i-2,dp);
+        
+        return dp[i];
     }
 }
