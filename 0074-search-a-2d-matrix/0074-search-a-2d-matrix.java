4000579fe83314fa210bebc117e872f963794de0
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-05-27 14:49:34 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (83.98%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        
+        if(m==0) return false;
+        int start=0;
+        int end=(m*n)-1;
+        
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(matrix[mid/n][mid%n]==target)
+            {
+                return true;
+            }
+            if(matrix[mid/n][mid%n]<target)
+            {
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        
+        return false;
+    }
+}
+
+    
+    
