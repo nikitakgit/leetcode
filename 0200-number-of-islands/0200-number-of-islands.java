f873e821cfbb8b9d68ab53112f8441f60eded894
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-05-30 17:35:54 +0530

    Time: 5 ms (26.24%), Space: 51.8 MB (15.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,62 @@
+
+class Pair {
+    int f;
+    int s;
+
+    public Pair(int f, int s) {
+        this.f = f;
+        this.s = s;
+    }
+}
+
 class Solution {
-        public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+    public void bfs(int r, int c, int[][] vis, char[][] grid) {
+        vis[r][c] = 1;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(r, c));
+        int n = grid.length;
+        int m = grid[0].length;
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+
+        while (!q.isEmpty()) {
+            Pair current = q.poll();
+            int pr = current.f;
+            int pc = current.s;
+
+            for (int i = 0; i < 4; i++) {
+                int nr = pr + dr[i];
+                int nc = pc + dc[i];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' && vis[nr][nc] == 0) {
+                    vis[nr][nc] = 1;
+                    q.add(new Pair(nr, nc));
                 }
             }
         }
-        
-        return numIslands;
     }
-    
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
-            return;
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
         }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
+
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] vis = new int[n][m];
+        int cnt = 0;
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                if (vis[r][c] == 0 && grid[r][c] == '1') {
+                    cnt++;
+                    bfs(r, c, vis, grid);
+                }
+            }
+        }
+
+        return cnt;
     }
 }
+
+
