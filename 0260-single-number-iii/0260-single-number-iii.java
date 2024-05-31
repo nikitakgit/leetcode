ef85b79a345e17e7ae59640944f3eda80d198926
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-05-31 10:41:24 +0530

    Time: 6 ms (31.65%), Space: 45.4 MB (81.95%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        ArrayList<Integer> arr=new ArrayList<>();
+        
+        int[] ans=new int[2];
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i:nums)
         {
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        
+        int ind=0;
         for(Map.Entry<Integer,Integer> e:map.entrySet())
         {
+            
             if(e.getValue()==1)
             {
-                arr.add(e.getKey());
+                ans[ind++]=e.getKey();
             }
         }
         
-        int[] result = new int[arr.size()];
-        for (int i = 0; i < arr.size(); i++) {
-            result[i] = arr.get(i);
-        }
-        return result;
+        
+        return ans;
     }
 }
