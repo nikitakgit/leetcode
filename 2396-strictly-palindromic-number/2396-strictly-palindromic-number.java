72749890d251915f4118ec90795dda75b3a25f41
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-06-20 21:47:12 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (36.62%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,5 +1,22 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+         boolean ans=false;
+       for(int i=2;i<n-2;i++){
+           int num=n;
+           int anum=0;
+           while(num!=0){
+               int rem=num%i;
+               anum=(anum*10)+(rem);
+               num=num/i;
+           }
+           if(anum==n){
+               ans=true;
+           }
+           else{
+               ans=false;
+               break;
+           }
+       }
+       return ans;
     }
 }
