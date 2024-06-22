7dfa206da99b10bb392c37c9d18cacf1052f7a50
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-06-22 17:58:33 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (68.06%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,12 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-      ListNode slow=head, fast=head;
-        while(fast!=null && fast.next!=null){
+      ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null)
+        {
             fast=fast.next.next;
             slow=slow.next;
-            if(fast==slow) return true;
+            if(fast==slow)
+            {
+                return true;
+            }
         }
-        return false;  
+        return false;
     }
 }
