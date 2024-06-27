a01daa5ac3fb5edeadf2bca8477c76ffe8e5ee86
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-06-27 13:02:01 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (12.62%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int p1=0,p2=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('){
-                p1++;
-                p2++;
-            } else if(s.charAt(i)==')'){
-                p1--;
-                p2--;
-            } else if(s.charAt(i)=='*'){
-                p1++;
-                p2--;
+        int min=0;
+        int max=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                min+=1;
+                max+=1;
+            }else if(s.charAt(i)==')')
+            {
+                min-=1;
+                max-=1;
+            }else{
+                min-=1;
+                max+=1;
             }
-            if(p1<0){
-                return false;
-            }
-            if(p2<0){
-                p2 = 0;
-            }
+            if(min<0) min=0;
+            if(max<0) return false;
         }
-        return p2==0;
+        return min==0;
     }
 }
