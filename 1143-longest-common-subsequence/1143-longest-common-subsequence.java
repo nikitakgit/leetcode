6376bc74d2d7090e13b23e910888e0baf39ad5c1
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-07-15 22:26:47 +0530

    Time: 24 ms (34.66%), Space: 51 MB (25.31%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,10 +3,11 @@ class Solution {
         int m=text1.length();
         int n=text2.length();
         int[][] dp=new int[m][n];
-        for(int[] rows:dp)
+        for(int[] d:dp)
         {
-            Arrays.fill(rows,-1);
+            Arrays.fill(d,-1);
         }
+        
         return helper(m-1,n-1,text1,text2,dp);
     }
     public int helper(int i,int j,String t1,String t2,int[][] dp)
@@ -14,13 +15,7 @@ class Solution {
         if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         
-        //if character matches
-        if(t1.charAt(i)==t2.charAt(j))
-        {
-            return dp[i][j]=1+helper(i-1,j-1,t1,t2,dp);
-        }
-        
-        //if character not matches
-        return dp[i][j]=Math.max(helper(i-1,j,t1,t2,dp),helper(i,j-1,t1,t2,dp));
+        if(t1.charAt(i)==t2.charAt(j)) return dp[i][j]=1+helper(i-1,j-1,t1,t2,dp);
+        return dp[i][j]=Math.max(helper(i,j-1,t1,t2,dp),helper(i-1,j,t1,t2,dp));
     }
 }
