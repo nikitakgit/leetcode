18f5d119e0f866c856c3d22f20c4359f895a753c
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-07-15 23:00:49 +0530

    Time: 119 ms (22.89%), Space: 42.3 MB (80.07%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -6,25 +6,28 @@ class Solution {
         if(dp[i][j]!=-1) return dp[i][j];
         for(int ind=i;ind<=j;ind++)
         {
-            int cost=(nums[i-1]*nums[ind]*nums[j+1])+helper(i,ind-1,nums,dp)+helper(ind+1,j,nums,dp);
+            int cost=nums[i-1]*nums[ind]*nums[j+1]+helper(i,ind-1,nums,dp)+helper(ind+1,j,nums,dp);
             maxi=Math.max(maxi,cost);
         }
         return dp[i][j]=maxi;
     }
-    public int maxCoins(int[] nums) {
-        int[] arr=new int[nums.length+2];
+    public int maxCoins(int[] nums)
+    {
+        int n=nums.length;
+        int[] arr=new int[n+2];
         arr[0]=1;
         arr[arr.length-1]=1;
         for(int i=1;i<arr.length-1;i++)
         {
             arr[i]=nums[i-1];
         }
-        int[][] dp=new int[nums.length+1][nums.length+1];
-        for(int[] row:dp)
+        
+        int[][] dp=new int[n+1][n+1];
+        for(int[] a:dp)
         {
-            Arrays.fill(row,-1);
+            Arrays.fill(a,-1);
         }
-        return helper(1,nums.length,arr,dp);
+        return helper(1,n,arr,dp);
     }
     
 }
