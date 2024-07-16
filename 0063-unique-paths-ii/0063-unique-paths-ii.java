80079596449afbe8035abd79e03b266725719720
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-07-16 20:53:28 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (83.14%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,38 +1,23 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] grid) {
-       int m=grid.length;
-        int n=grid[0].length;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
         int[][] dp=new int[m][n];
-        for(int[] rows:dp)
+        for(int[] a:dp)
         {
-            Arrays.fill(rows,-1);
+            Arrays.fill(a,-1);
         }
-        
-        return solve(grid,m-1,n-1,dp);
+        return helper(m-1,n-1,obstacleGrid,dp);
     }
-    public int solve(int[][] grid,int i,int j,int[][] dp)
+    public int helper(int i,int j,int[][] grid,int[][] dp)
     {
-        if(i>=0 && j>=0 && grid[i][j]==1)
-        {
-            return 0;
-        }
-        if(i==0 && j==0)
-        {
-            return 1;
-        }
-        if(i<0 || j<0)
-        {
-            return 0;
-        }
+        if(i>=0 && j>=0 && grid[i][j]==1) return 0;
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
         
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        
-        int up=solve(grid,i-1,j,dp);
-        int left=solve(grid,i,j-1,dp);
-        
+        int up=helper(i-1,j,grid,dp);
+        int left=helper(i,j-1,grid,dp);
         return dp[i][j]=up+left;
     }
 }
