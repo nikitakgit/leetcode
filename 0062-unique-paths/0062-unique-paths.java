f506ffed17ed38b002bc4f6f8f4651702c2b710e
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-07-16 20:29:48 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (64.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,35 +1,20 @@
 class Solution {
-    int[][] dp;
-
     public int uniquePaths(int m, int n) {
-        dp=new int[m][n];
-        for(int[] rows:dp)
+        int[][] dp=new int[m][n];
+        for(int[] a:dp)
         {
-            Arrays.fill(rows,-1);
+            Arrays.fill(a,-1);
         }
-        return solve(m-1,n-1,dp);
-       
-        
-}
-    public int solve(int i,int j,int[][] dp)
+        return helper(m-1,n-1,dp);
+    }
+    public int helper(int i,int j,int[][] dp)
     {
-        if(i==0 && j==0)
-        {
-            return 1;
-        }
-        if(i<0 || j<0)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=helper(i-1,j,dp);
+        int left=helper(i,j-1,dp);
         
-        int up=solve(i-1,j,dp);
-        int left=solve(i,j-1,dp);
-        
-        dp[i][j]=up+left;
-        return dp[i][j];
+        return dp[i][j]=up+left;
     }
 }
