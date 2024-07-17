a197bfe94f11458c03a6f3b5fcce70672ff2e492
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-07-17 22:22:02 +0530

    Time: 2 ms (73.86%), Space: 42.2 MB (66.66%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,32 +1,32 @@
 class Pair{
     int r;
     int c;
-    int time;
+    int t;
     
-    Pair(int r,int c,int time)
+    public Pair(int r,int c,int t)
     {
         this.r=r;
         this.c=c;
-        this.time=time;
+        this.t=t;
     }
 }
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
+     int m=grid.length;
+     int n=grid[0].length;
+     int[][] vis=new int[m][n];
+     
         Queue<Pair> q=new LinkedList<>();
-        int[][] vis=new int[n][m];
         int cntfresh=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==2)
                 {
                     q.add(new Pair(i,j,0));
                     vis[i][j]=2;
-                }
-                else{
+                }else{
                     vis[i][j]=0;
                 }
                 if(grid[i][j]==1)
@@ -37,25 +37,25 @@ class Solution {
         }
         
         int tm=0;
-        int[] drow={-1,0,1,0};
-        int[] dcol={0,1,0,-1};
-        
+        int[] dr={-1,0,1,0};
+        int[] dc={0,1,0,-1};
         int cnt=0;
+        
         while(!q.isEmpty())
         {
-            Pair curr=q.poll();
-            int pr=curr.r;
-            int pc=curr.c;
-            int t=curr.time;
-            tm=Math.max(tm,t);
+            Pair p=q.poll();
+            int r1=p.r;
+            int c1=p.c;
+            int t1=p.t;
+            tm=Math.max(tm,t1);
             for(int i=0;i<4;i++)
             {
-                int nr=pr+drow[i];
-                int nc=pc+dcol[i];
+                int nr=r1+dr[i];
+                int nc=c1+dc[i];
                 
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0 && grid[nr][nc]==1)
+                if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==0 && grid[nr][nc]==1)
                 {
-                    q.add(new Pair(nr,nc,t+1));
+                    q.add(new Pair(nr,nc,t1+1));
                     vis[nr][nc]=2;
                     cnt++;
                 }
@@ -63,6 +63,7 @@ class Solution {
         }
         if(cnt!=cntfresh) return -1;
         return tm;
+     
     }
     
 }
