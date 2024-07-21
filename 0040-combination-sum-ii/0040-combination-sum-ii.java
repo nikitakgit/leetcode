84bf9233f43a1e04d8b858a27a9fa6e96c9bbd98
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-07-21 23:00:45 +0530

    Time: 2 ms (99.54%), Space: 43 MB (79.77%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
+       List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(candidates);
-        findCombination(0,candidates,target,ans,new ArrayList<>());
+        helper(0,candidates,candidates.length,target,ans,new ArrayList<>());
         return ans;
     }
-    private void findCombination(int ind,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds)
+    public void helper(int ind,int[] candidates,int n,int target,List<List<Integer>> ans,List<Integer> ds)
     {
-            if(target==0)
-            {
-                ans.add(new ArrayList<>(ds));
-                return;
-            }
-                 
-        for(int i=ind;i<arr.length;i++)
+        if(target==0)
         {
-            if(i>ind && arr[i]==arr[i-1]) continue;
-            if(arr[i]>target) break;
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        for(int i=ind;i<n;i++)
+        {
+            if(i>ind && candidates[i-1]==candidates[i]) continue;
+            if(candidates[i]>target) break;
             
-            ds.add(arr[i]);
-            findCombination(i+1,arr,target-arr[i],ans,ds);
+            ds.add(candidates[i]);
+            helper(i+1,candidates,n,target-candidates[i],ans,ds);
             ds.remove(ds.size()-1);
         }
     }
