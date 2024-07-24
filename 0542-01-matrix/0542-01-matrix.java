41ddb7d78f2363d0fa26f08eb5412f341d4f2d8c
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-07-24 23:30:10 +0530

    Time: 13 ms (81.58%), Space: 47.2 MB (55.76%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,8 +2,7 @@ class Pair{
     int f;
     int s;
     int t;
-    
-    public Pair(int f,int s,int t)
+    Pair(int f,int s,int t)
     {
         this.f=f;
         this.s=s;
@@ -12,47 +11,45 @@ class Pair{
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int n=mat.length;
-        int m=mat[0].length;
-        
-        int[][] vis=new int[n][m];
-        int[][] dist=new int[n][m];
-        
+       int m=mat.length;
+        int n=mat[0].length;
+        int[][] vis=new int[m][n];
+        int[][] dist=new int[m][n];
         Queue<Pair> q=new LinkedList<>();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
                 if(mat[i][j]==0)
                 {
-                    q.add(new Pair(i,j,0));
                     vis[i][j]=1;
+                    q.add(new Pair(i,j,0));
                 }else{
                     vis[i][j]=0;
                 }
             }
         }
         
-        int[] delrow={-1,0,1,0};
-        int[] delcol={0,1,0,-1};
+        int[] delRow={-1,0,1,0};
+        int[] delCol={0,1,0,-1};
         
         while(!q.isEmpty())
         {
-            Pair curr=q.poll();
-            int r=curr.f;
-            int c=curr.s;
-            int step=curr.t;
+            Pair p=q.poll();
+            int row=p.f;
+            int col=p.s;
+            int step=p.t;
             
-            dist[r][c]=step;
+            dist[row][col]=step;
             for(int i=0;i<4;i++)
             {
-                int nr=r+delrow[i];
-                int nc=c+delcol[i];
+                int nrow=row+delRow[i];
+                int ncol=col+delCol[i];
                 
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0)
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0)
                 {
-                    vis[nr][nc]=1;
-                    q.add(new Pair(nr,nc,step+1));
+                    vis[nrow][ncol]=1;
+                    q.add(new Pair(nrow,ncol,step+1));
                 }
             }
         }
