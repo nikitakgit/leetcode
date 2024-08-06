fc30a92756025a76a657473aa06b5a02a7391f4b
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-08-06 23:35:19 +0530

    Time: 1 ms (33.08%), Space: 42.6 MB (5.80%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,30 +1,33 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int n = s.length();
-        // dp[i][cnt] indicates whether the string from index i to n can be valid with cnt '(' minus ')'
-        Boolean[][] dp = new Boolean[n + 1][n + 1];
-        return helper(s, 0, 0, dp);
+      
+        int n=s.length();
+        Boolean[][] dp=new Boolean[n+1][n+1];
+        return helper(s,0,0,dp);
     }
-
-    private boolean helper(String s, int i, int cnt, Boolean[][] dp) {
-        if (cnt < 0) return false; // More closing brackets than opening ones
-        if (i == s.length()) {
-            return cnt == 0; // All opening brackets should be matched with closing brackets
-        }
-        if (dp[i][cnt] != null) {
-            return dp[i][cnt];
-        }
-
+    public boolean helper(String s,int i,int cnt,Boolean[][] dp)
+    {
         boolean result;
-        if (s.charAt(i) == '(') {
-            result = helper(s, i + 1, cnt + 1, dp);
-        } else if (s.charAt(i) == ')') {
-            result = helper(s, i + 1, cnt - 1, dp);
-        } else { // s.charAt(i) == '*'
-            result = helper(s, i + 1, cnt + 1, dp) || helper(s, i + 1, cnt, dp) || helper(s, i + 1, cnt - 1, dp);
+        if(cnt<0) return false;
+        if(i==s.length())
+        {
+            return cnt==0;
         }
-
-        dp[i][cnt] = result;
+        if(dp[i][cnt]!=null) return dp[i][cnt];
+        if(s.charAt(i)=='(')
+        {
+            result= helper(s,i+1,cnt+1,dp);
+        }
+        else if(s.charAt(i)==')')
+        {
+            result= helper(s,i+1,cnt-1,dp);
+        }else{
+            
+        
+        result= helper(s,i+1,cnt+1,dp) || helper(s,i+1,cnt,dp) || helper(s,i+1,cnt-1,dp);
+        
+        }
+        dp[i][cnt]=result;
         return result;
     }
 }
