a167f3cf2e03e04d3ec81534b3bb300ef701ea75
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-08-06 23:30:35 +0530

    Time: 1 ms (33.08%), Space: 42.5 MB (5.80%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,24 +1,30 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int min=0;
-        int max=0;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)=='(')
-            {
-                min+=1;
-                max+=1;
-            }else if(s.charAt(i)==')')
-            {
-                min-=1;
-                max-=1;
-            }else{
-                min-=1;
-                max+=1;
-            }
-            if(min<0) min=0;
-            if(max<0) return false;
+        int n = s.length();
+        // dp[i][cnt] indicates whether the string from index i to n can be valid with cnt '(' minus ')'
+        Boolean[][] dp = new Boolean[n + 1][n + 1];
+        return helper(s, 0, 0, dp);
+    }
+
+    private boolean helper(String s, int i, int cnt, Boolean[][] dp) {
+        if (cnt < 0) return false; // More closing brackets than opening ones
+        if (i == s.length()) {
+            return cnt == 0; // All opening brackets should be matched with closing brackets
         }
-        return min==0;
+        if (dp[i][cnt] != null) {
+            return dp[i][cnt];
+        }
+
+        boolean result;
+        if (s.charAt(i) == '(') {
+            result = helper(s, i + 1, cnt + 1, dp);
+        } else if (s.charAt(i) == ')') {
+            result = helper(s, i + 1, cnt - 1, dp);
+        } else { // s.charAt(i) == '*'
+            result = helper(s, i + 1, cnt + 1, dp) || helper(s, i + 1, cnt, dp) || helper(s, i + 1, cnt - 1, dp);
+        }
+
+        dp[i][cnt] = result;
+        return result;
     }
 }
