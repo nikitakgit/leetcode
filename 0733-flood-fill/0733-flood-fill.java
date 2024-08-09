cf69b197ee1dbc7cc765d37246c335caf4102c67
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-08-09 12:05:47 +0530

    Time: 1 ms (45.15%), Space: 45 MB (17.72%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,26 +1,28 @@
 class Solution {
-    public void dfs(int r,int c,int[][] img,int[][] ans, int newColor,int[] dr,int[] dc,int iniColor)
+   
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+       int iniCol=image[sr][sc];
+        int[][] ans=image;
+        int[] dr={-1,0,1,0};
+        int[] dc={0,1,0,-1};
+        dfs(sr,sc,ans,image,color,dr,dc,iniCol);
+        return ans;
+    }
+    public void dfs(int r,int c,int[][] ans,int[][] image,int newCol,int[] dr,int[] dc,int iniCol)
     {
-        ans[r][c]=newColor;
-        int n=img.length;
-        int m=img[0].length;
+        ans[r][c]=newCol;
+        int n=image.length;
+        int m=image[0].length;
+        
         for(int i=0;i<4;i++)
         {
             int nr=r+dr[i];
             int nc=c+dc[i];
             
-            if(nr>=0 && nr<n && nc>=0 && nc<m && img[nr][nc]==iniColor && ans[nr][nc]!=newColor)
+            if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==iniCol && ans[nr][nc]!=newCol)
             {
-                dfs(nr,nc,img,ans,newColor,dr,dc,iniColor);
+                dfs(nr,nc,ans,image,newCol,dr,dc,iniCol);
             }
         }
-    }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int iniColor=image[sr][sc];
-        int[][] ans=image;
-        int[] dr={-1,0,1,0};
-        int[] dc={0,1,0,-1};
-        dfs(sr,sc,image,ans,color,dr,dc,iniColor);
-        return ans;
     }
 }
