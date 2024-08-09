034e1fccd8d3b2b6f6e3c84955f66cedc321f201
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-08-09 20:52:07 +0530

    Time: 2 ms (86.94%), Space: 45.1 MB (82.58%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,47 +1,33 @@
 class Solution {
    
-    public void dfs(int r,int c,int[][] vis,char[][] board,int[] delrow,int[] delcol)
-    {
-        vis[r][c]=1;
-        int m=board.length;
+    
+    public void solve(char[][] board) {
+       int m=board.length;
         int n=board[0].length;
-        for(int i=0;i<4;i++)
+        int[][] vis=new int[m][n];
+        int[] dr={-1,0,1,0};
+        int[] dc={0,1,0,-1};
+        for(int i=0;i<m;i++)
         {
-            int nrow=r+delrow[i];
-            int ncol=c+delcol[i];
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && board[nrow][ncol]=='O')
+            if(vis[i][0]==0 && board[i][0]=='O' )
             {
-                dfs(nrow,ncol,vis,board,delrow,delcol);
+                dfs(i,0,board,vis,dr,dc);
+            }
+            if(vis[i][n-1]==0 && board[i][n-1]=='O')
+            {
+                dfs(i,n-1,board,vis,dr,dc);
             }
         }
-    }
-    public void solve(char[][] board) {
-        int m=board.length;
-        int n=board[0].length;
-      int[] delrow={-1,0,1,0};
-        int[] delcol={0,1,0,-1};
-        int[][] vis=new int[m][n];
         
         for(int j=0;j<n;j++)
         {
             if(vis[0][j]==0 && board[0][j]=='O')
             {
-                dfs(0,j,vis,board,delrow,delcol);
+                dfs(0,j,board,vis,dr,dc);
             }
             if(vis[m-1][j]==0 && board[m-1][j]=='O')
             {
-                dfs(m-1,j,vis,board,delrow,delcol);
-            }
-        }
-        for(int i=0;i<m;i++)
-        {
-            if(vis[i][0]==0 && board[i][0]=='O')
-            {
-                dfs(i,0,vis,board,delrow,delcol);
-            }
-            if(vis[i][n-1]==0 && board[i][n-1]=='O')
-            {
-                dfs(i,n-1,vis,board,delrow,delcol);
+                dfs(m-1,j,board,vis,dr,dc);
             }
         }
         
@@ -49,12 +35,30 @@ class Solution {
         {
             for(int j=0;j<n;j++)
             {
-                if(vis[i][j]==0 && board[i][j]=='O')
+                if(board[i][j]=='O' && vis[i][j]==0)
                 {
                     board[i][j]='X';
                 }
             }
         }
         
+    }
+    
+    public void dfs(int r,int c,char[][] board,int[][] vis,int[] dr,int[] dc)
+    {
+        vis[r][c]=1;
+        int m=board.length;
+        int n=board[0].length;
+        
+        for(int i=0;i<4;i++)
+        {
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            
+            if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==0 && board[nr][nc]=='O')
+            {
+                dfs(nr,nc,board,vis,dr,dc);
+            }
+        }
     }
 }
