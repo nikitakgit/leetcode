33c014d10bade0d24d8f9c211a3fee072d1aae07
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-08-09 14:42:30 +0530

    Time: 2 ms (72.12%), Space: 42 MB (90.91%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -13,11 +13,11 @@ class Pair{
 class Solution {
     public int orangesRotting(int[][] grid) {
      int m=grid.length;
-     int n=grid[0].length;
-     int[][] vis=new int[m][n];
-     
+        int n=grid[0].length;
+        int[][] vis=new int[m][n];
+        
         Queue<Pair> q=new LinkedList<>();
-        int cntfresh=0;
+        int cntFresh=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -31,7 +31,7 @@ class Solution {
                 }
                 if(grid[i][j]==1)
                 {
-                    cntfresh++;
+                    cntFresh++;
                 }
             }
         }
@@ -40,28 +40,30 @@ class Solution {
         int[] dr={-1,0,1,0};
         int[] dc={0,1,0,-1};
         int cnt=0;
-        
         while(!q.isEmpty())
         {
             Pair p=q.poll();
-            int r1=p.r;
-            int c1=p.c;
-            int t1=p.t;
-            tm=Math.max(tm,t1);
+            int r=p.r;
+            int c=p.c;
+            int t=p.t;
+            
+            tm=Math.max(tm,t);
+            
             for(int i=0;i<4;i++)
             {
-                int nr=r1+dr[i];
-                int nc=c1+dc[i];
+                int nr=r+dr[i];
+                int nc=c+dc[i];
                 
                 if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==0 && grid[nr][nc]==1)
                 {
-                    q.add(new Pair(nr,nc,t1+1));
+                    q.add(new Pair(nr,nc,t+1));
                     vis[nr][nc]=2;
                     cnt++;
                 }
             }
+            
         }
-        if(cnt!=cntfresh) return -1;
+        if(cnt!=cntFresh) return -1;
         return tm;
      
     }
