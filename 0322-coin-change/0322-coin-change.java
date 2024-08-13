12e383b377550744bedb39fbb777dd936a6102ce
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-08-13 17:09:21 +0530

    Time: 36 ms (18.02%), Space: 45.5 MB (12.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,11 +1,12 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-  int[][] dp=new int[coins.length][amount+1];
-        for(int[] row:dp)
+        int n=coins.length;
+        int[][] dp=new int[n][amount+1];
+        for(int[] a:dp)
         {
-            Arrays.fill(row,-1);
+            Arrays.fill(a,-1);
         }
-        int ans= f(coins.length-1,coins,amount,dp);
+        int ans=helper(n-1,coins,amount,dp);
         if(ans>=(int)Math.pow(10,9))
         {
             return -1;
@@ -13,31 +14,23 @@ class Solution {
         return ans;
     }
     
-    public int f(int i,int[] coins,int target,int[][] dp)
-    {   
-        
-        if(i==0)
+    public int helper(int ind,int[] coins,int target,int[][] dp)
+    {
+        if(ind==0)
         {
-            if(target%coins[i]==0)
-            {
-                return target/coins[0];
-            }
+            if(target%coins[ind]==0) return target/coins[ind];
             else return (int)Math.pow(10,9);
         }
-        if(dp[i][target]!=-1)
+        if(dp[ind][target]!=-1)
         {
-            return dp[i][target];
+            return dp[ind][target];
         }
-        int notTake=0+f(i-1,coins,target,dp);
-        int Take=Integer.MAX_VALUE;
-        if(coins[i]<=target)
+        int notTake=helper(ind-1,coins,target,dp);
+        int take=Integer.MAX_VALUE;
+        if(coins[ind]<=target)
         {
-            Take=1+f(i,coins,target-coins[i],dp);
+            take=1+helper(ind,coins,target-coins[ind],dp);
         }
-        
-         dp[i][target]=Math.min(Take,notTake);
-        return dp[i][target];
-        
-    
+        return dp[ind][target]=Math.min(take,notTake);
     }
 }
