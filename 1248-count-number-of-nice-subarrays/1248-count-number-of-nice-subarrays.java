b5120877e2e55ec9f8e223081bf8554afdc27b9a
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-08-21 21:25:18 +0530

    Time: 11 ms (46.22%), Space: 54.9 MB (37.52%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -4,15 +4,15 @@ class Solution {
     }
     public int helper(int[] nums,int k)
     {
-        if(k<0) return 0;
         int l=0,r=0,sum=0,cnt=0;
+        if(k<0) return 0;
         while(r<nums.length)
         {
-            sum+=nums[r]%2;
+            sum+=(nums[r]%2);
             while(sum>k)
             {
-                sum-=nums[l]%2;
-                l=l+1;
+                sum=sum-(nums[l]%2);
+                l++;
             }
             cnt+=(r-l+1);
             r++;
