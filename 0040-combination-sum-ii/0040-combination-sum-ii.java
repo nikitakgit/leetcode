0376d8a3684ed126012e3b9c30bc1902017d1149
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-08-26 16:52:29 +0530

    Time: 3 ms (78.06%), Space: 43.5 MB (17.33%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,26 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-       List<List<Integer>> ans=new ArrayList<>();
-        Arrays.sort(candidates);
-        helper(0,candidates,candidates.length,target,ans,new ArrayList<>());
+       Arrays.sort(candidates);
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(0,candidates.length,candidates,target,new ArrayList<>(),ans);
         return ans;
     }
-    public void helper(int ind,int[] candidates,int n,int target,List<List<Integer>> ans,List<Integer> ds)
+    public void helper(int ind,int n,int[] candidates,int target,List<Integer> ds,List<List<Integer>> ans)
     {
-        if(target==0)
-        {
-            ans.add(new ArrayList<>(ds));
+        
+            if(target==0) {
+                ans.add(new ArrayList<>(ds));
             return;
-        }
+            }
+        
         
         for(int i=ind;i<n;i++)
         {
-            if(i>ind && candidates[i-1]==candidates[i]) continue;
+            if(i>ind && candidates[i]==candidates[i-1]) continue;
             if(candidates[i]>target) break;
             
             ds.add(candidates[i]);
-            helper(i+1,candidates,n,target-candidates[i],ans,ds);
+            helper(i+1,n,candidates,target-candidates[i],ds,ans);
             ds.remove(ds.size()-1);
         }
+        
     }
 }
