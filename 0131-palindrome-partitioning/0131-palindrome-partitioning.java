519db7f9707c6337ee3b3cce36393f2fab2f5f1f
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-08-26 22:02:01 +0530

    Time: 7 ms (98.36%), Space: 56.9 MB (42.42%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<List<String>> partition(String s) {
-      List<List<String>> ans=new ArrayList<>();
+       List<List<String>> ans=new ArrayList<>();
         helper(0,s,new ArrayList<>(),ans);
         return ans;
     }
-    public void helper(int ind,String s,List<String> path,List<List<String>> ans)
+    
+    public void helper(int index,String s,List<String> path,List<List<String>> ans)
     {
-        if(ind==s.length())
+        if(index==s.length())
         {
             ans.add(new ArrayList<>(path));
             return;
         }
         
-        for(int i=ind;i<s.length();i++)
+        for(int i=index;i<s.length();i++)
         {
-            if(isPalindrome(s,ind,i))
+            if(isPalindrome(s,index,i))
             {
-                path.add(s.substring(ind,i+1));
+                path.add(s.substring(index,i+1));
                 helper(i+1,s,path,ans);
                 path.remove(path.size()-1);
             }
         }
-        
     }
     public boolean isPalindrome(String s,int start,int end)
     {
         while(start<=end)
         {
-            if(s.charAt(start++)!=s.charAt(end--))
-            {
-                return false;
-            }
+            if(s.charAt(start++)!=s.charAt(end--)) return false;
         }
         return true;
     }
