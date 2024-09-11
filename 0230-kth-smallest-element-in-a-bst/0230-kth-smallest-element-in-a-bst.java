9cb889bc244bb7dd3eae72183939062778269ddf
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-09-12 01:20:48 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (77.48%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -16,22 +16,23 @@
 class Solution {
     
 
-    private void inorder(TreeNode root,int k,int[] cnt,int[] kthSmallest) {
+    private void inOrder(TreeNode root,int k,int[] cnt,int[] kthSmallest)
+    {
         if(root==null || cnt[0]>=k) return;
-        inorder(root.left,k,cnt,kthSmallest);
+        inOrder(root.left,k,cnt,kthSmallest);
         cnt[0]++;
         if(cnt[0]==k)
         {
             kthSmallest[0]=root.val;
             return;
         }
-        inorder(root.right,k,cnt,kthSmallest);
+        inOrder(root.right,k,cnt,kthSmallest);
     }
 
     public int kthSmallest(TreeNode root, int k) {
         int[] cnt=new int[]{0};
         int[] kthSmallest=new int[]{Integer.MIN_VALUE};
-        inorder(root,k,cnt,kthSmallest);
+        inOrder(root,k,cnt,kthSmallest);
         return kthSmallest[0];
     }
 }
