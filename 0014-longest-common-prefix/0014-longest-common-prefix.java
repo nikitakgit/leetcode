6620b607e02555d5fc753fbed2146601ca3b8c38
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-09-13 12:01:49 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (67.05%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,14 +4,15 @@ class Solution {
         {
             return "";
         }
-
+        
         String prefix=strs[0];
         for(int i=1;i<strs.length;i++)
         {
             while(!strs[i].startsWith(prefix))
             {
-                prefix = prefix.substring(0,prefix.length()-1);
-                if(prefix.isEmpty()){
+                prefix=prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty())
+                {
                     return "";
                 }
             }
