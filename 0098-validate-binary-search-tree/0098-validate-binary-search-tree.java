5e9ed1aa46388a3074c2444a46001a255db524b3
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-09-19 00:37:05 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (64.08%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -17,10 +17,10 @@ class Solution {
     public boolean isValidBST(TreeNode root) {
       return validBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
-    public boolean validBST(TreeNode root,long minVal,long maxVal)
+    public boolean validBST(TreeNode root,long min,long max)
     {
         if(root==null) return true;
-        if(root.val>=maxVal || root.val<=minVal) return false;
-        return validBST(root.left,minVal,root.val) && validBST(root.right,root.val,maxVal);
+        if(root.val<=min || root.val>=max) return false;
+        return validBST(root.left,min,root.val) && validBST(root.right,root.val,max);
     }
 }
