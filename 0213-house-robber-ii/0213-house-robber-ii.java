29fe75b893d7868e2e077091bdc8eb7f6d7e8d27
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-09-18 16:14:38 +0530

    Time: 1 ms (9.29%), Space: 41.1 MB (27.83%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,16 +1,15 @@
 class Solution {
     public int rob(int[] valInHouse) {
-        ArrayList<Integer> temp1=new ArrayList<>();
-        ArrayList<Integer> temp2=new ArrayList<>();
-        
         int n=valInHouse.length;
         if(n==1) return valInHouse[0];
-        for(int i=0;i<n;i++)
-        {
-            if(i!=0) temp1.add(valInHouse[i]);
-            if(i!=n-1) temp2.add(valInHouse[i]);
-        }
-        
+       ArrayList<Integer> temp1=new ArrayList<>();
+       ArrayList<Integer> temp2=new ArrayList<>();
+       for(int i=0;i<n;i++)
+       {
+           if(i!=0) temp1.add(valInHouse[i]);
+           if(i!=n-1) temp2.add(valInHouse[i]);
+       }
+       
         int[] arr1=new int[temp1.size()];
         int[] arr2=new int[temp2.size()];
         for(int i=0;i<temp1.size();i++)
@@ -26,18 +25,21 @@ class Solution {
         
         Arrays.fill(dp1,-1);
         Arrays.fill(dp2,-1);
+        
         return Math.max(helper(arr1.length-1,arr1,dp1),helper(arr2.length-1,arr2,dp2));
+        
     }
-    public int helper(int i,int[] nums,int[] dp)
+    public int helper(int i,int[] arr,int[] dp)
     {
-        if(i==0) return nums[0];
+        if(i==0) return arr[0];
         if(dp[i]!=-1) return dp[i];
-        int pick=nums[i];
-        int notpick=0+helper(i-1,nums,dp);
+        
+        int pick=arr[i];
+        int notPick=0+helper(i-1,arr,dp);
         if(i>1)
         {
-            pick+=helper(i-2,nums,dp);
+            pick+=helper(i-2,arr,dp);
         }
-        return dp[i]=Math.max(pick,notpick);
+        return dp[i]=Math.max(pick,notPick);
     }
 }
