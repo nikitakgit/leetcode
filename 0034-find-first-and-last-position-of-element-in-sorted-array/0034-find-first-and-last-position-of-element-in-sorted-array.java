f3285ab83b1f76cc3a288b1253a00a0c8d582369
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-09-21 23:38:07 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (32.68%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans={-1,-1};
-        ans[0]=find(nums,target,true);
+       int[] ans={-1,-1};
+        ans[0]=findFirstAndLast(nums,target,true);
         if(ans[0]!=-1)
         {
-            ans[1]=find(nums,target,false);
+            ans[1]=findFirstAndLast(nums,target,false);
         }
         return ans;
     }
-    public int find(int[] nums,int target,boolean findFirst)
+    public int findFirstAndLast(int[] nums,int target,boolean findFirst)
     {
-        int s=0;
-        int e=nums.length-1;
+        int l=0;
+        int h=nums.length-1;
         int ans=-1;
-        while(s<=e)
+        while(l<=h)
         {
-            int mid=s+(e-s)/2;
+            int mid=l+(h-l)/2;
             if(nums[mid]<target)
             {
-                s=mid+1;
+                l=mid+1;
             }else if(nums[mid]>target)
             {
-                e=mid-1;
+                h=mid-1;
             }else{
                 ans=mid;
                 if(findFirst)
                 {
-                    e=mid-1;
+                    h=mid-1;
                 }else{
-                    s=mid+1;
+                    l=mid+1;
                 }
             }
         }
