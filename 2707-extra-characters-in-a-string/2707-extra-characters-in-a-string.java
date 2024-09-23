7716e5240cc9d8eb4392e656c0310c8b199669ce
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-09-23 21:21:59 +0530

    Time: 49 ms (23.81%), Space: 45.4 MB (36.39%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,47 +1,33 @@
 class Solution {
-    class TrieNode {
-        TrieNode[] child;
-        boolean isEnd;
-
-        TrieNode() {
-            child = new TrieNode[26];
-            for (int i = 0; i < 26; i++)
-                child[i] = null;
-            isEnd = false;
-        }
-    }
+    
 
     public int minExtraChar(String s, String[] dictionary) {
-        int n = s.length();
-        TrieNode root = new TrieNode();
-        for (String word : dictionary) {
-            TrieNode curr = root;
-            for (int i = 0; i < word.length(); ++i) {
-                int idx = word.charAt(i) - 'a';
-                if (curr.child[idx] == null)
-                    curr.child[idx] = new TrieNode();
-                curr = curr.child[idx];
-            }
-            curr.isEnd = true;
+        int n=s.length();
+        Set<String> dict=new HashSet<>();
+
+        for(String word: dictionary)
+        {
+            dict.add(word);
         }
-        int[] dp = new int[n + 1];
-        for (int i = n - 1; i >= 0; i--) {
-            ArrayList<Integer> temp = new ArrayList<>();
-            TrieNode node = root;
-            int j = i;
-            while (j < n) {
-                int idx = s.charAt(j) - 'a';
-                if (node.child[idx] == null)
-                    break;
-                node = node.child[idx];
-                if (node.isEnd)
-                    temp.add(j);
-                j++;
+
+        int[] dp=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            dp[i]=dict.contains(s.substring(0,i+1))?0:i+1;
+
+            for(int j=0;j<i;j++)
+            {
+                String partition=s.substring(j+1,i+1);
+
+                if(dict.contains(partition))
+                {
+                    dp[i]=Math.min(dp[i],dp[j]);
+                }else{
+                    dp[i]=Math.min(dp[i],dp[j]+i-j);
+                }
             }
-            dp[i] = 1 + dp[i + 1];
-            for (int x : temp)
-                dp[i] = Math.min(dp[i], dp[x + 1]);
         }
-        return dp[0];
+        return dp[n-1];
     }
 }
