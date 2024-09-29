033b43707c4f6950386fbf15ea411f619dec168b
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-09-29 22:10:51 +0530

    Time: 51 ms (77.76%), Space: 60.1 MB (78.77%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.java
@@ -0,0 +1,79 @@
+class AllOne {
+
+        
+    public class node{
+        String name="";
+        int val=0;
+        int pos=0;
+
+        node(String s,int key,int pos)
+        {
+          name=s;
+          val=key;
+          this.pos=pos;
+        }
+    }
+
+    List<node> l=new ArrayList<>();
+    Map<String,node> map=new HashMap<>();
+
+    public AllOne() { 
+    }
+
+    public void inc(String key) {
+        if(map.containsKey(key))
+        {
+            node temp=map.get(key);
+            temp.val++;
+            int pos=temp.pos;
+            while(--pos>=0 && l.get(pos).val<temp.val)
+            {
+                l.get(pos).pos=pos+1;
+                Collections.swap(l,pos+1,pos);
+                temp.pos=pos;
+            }
+        }
+        else
+        {
+            node temp=new node(key,1,l.size());
+            l.add(temp);
+            map.put(key,temp);
+        }
+    }
+    
+    public void dec(String key) {
+        node temp=map.get(key);
+        temp.val--;
+        int pos=temp.pos;
+
+        while(++pos<l.size() && l.get(pos).val>temp.val)
+        {
+            l.get(pos).pos=pos-1;
+            Collections.swap(l,pos,pos-1);
+            l.get(pos).pos=pos;
+        }
+
+        if(temp.val==0)
+        {
+            l.remove(pos-1);
+            map.remove(key);
+        }
+    }
+    
+    public String getMaxKey() {
+        return l.size()==0?"":l.get(0).name;
+    }
+    
+    public String getMinKey() {
+        return l.size()==0?"":l.get(l.size()-1).name;
+    }
+}
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne obj = new AllOne();
+ * obj.inc(key);
+ * obj.dec(key);
+ * String param_3 = obj.getMaxKey();
+ * String param_4 = obj.getMinKey();
+ */
