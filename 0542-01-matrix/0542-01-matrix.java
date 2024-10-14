c72160ad861c8ef3846f5bf108fa5d7dae7c98b7
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-10-15 00:10:24 +0530

    Time: 12 ms (90.29%), Space: 47.1 MB (65.37%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,22 +1,22 @@
 class Pair{
-    int f;
-    int s;
-    int t;
-    Pair(int f,int s,int t)
+    int r;
+    int c;
+    int d;
+    Pair(int r,int c,int d)
     {
-        this.f=f;
-        this.s=s;
-        this.t=t;
+        this.r=r;
+        this.c=c;
+        this.d=d;
     }
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-       int m=mat.length;
+        int m=mat.length;
         int n=mat[0].length;
         int[][] vis=new int[m][n];
         int[][] dis=new int[m][n];
+       Queue<Pair> q=new LinkedList<>();
         
-        Queue<Pair> q=new LinkedList<>();
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -36,23 +36,21 @@ class Solution {
         while(!q.isEmpty())
         {
             Pair p=q.poll();
-            int r=p.f;
-            int c=p.s;
-            int step=p.t;
+            int r=p.r;
+            int c=p.c;
+            int d=p.d;
+            dis[r][c]=d;
             
-            dis[r][c]=step;
             for(int i=0;i<4;i++)
             {
-                int nr=r+dr[i];
-                int nc=c+dc[i];
-                
-                if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==0)
-                {
-                    vis[nr][nc]=1;
-                    q.add(new Pair(nr,nc,step+1));
-                }
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(nr>=0 && nc>=0 && nr<m && nc<n && vis[nr][nc]==0)
+            {
+                q.add(new Pair(nr,nc,d+1));
+                vis[nr][nc]=1;
             }
-            
+            }
         }
         return dis;
     }
