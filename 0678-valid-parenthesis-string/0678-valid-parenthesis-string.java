47e027c9af63fb19066e04951ece86bc39e6f74d
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-10-16 21:06:47 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (34.28%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean checkValidString(String s) {
-      
       int mini=0;
         int maxi=0;
+        
         for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)=='(')
@@ -21,7 +21,6 @@ class Solution {
             if(mini<0) mini=0;
             if(maxi<0) return false;
         }
-        
         return mini==0;
     }
 }
