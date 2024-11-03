47baf7c22d7705f02db8201394b41f48b6d7216e
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-11-03 21:37:50 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (34.03%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -7,11 +7,11 @@ class Solution {
             if(nums[mid]==target)
             {
                 return mid;
-            }else if(nums[mid]>target)
+            }else if(target>nums[mid])
             {
-                r=mid-1;
-            }else{
                 l=mid+1;
+            }else{
+                r=mid-1;
             }
         }
         return -1;
