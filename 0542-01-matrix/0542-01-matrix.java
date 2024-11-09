548e7cdce3640bd0bc51f0e81394eefaffe61936
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-11-10 00:55:29 +0530

    Time: 13 ms (85.98%), Space: 47.1 MB (65.03%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -13,10 +13,11 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m=mat.length;
         int n=mat[0].length;
+        
         int[][] vis=new int[m][n];
         int[][] dis=new int[m][n];
-       Queue<Pair> q=new LinkedList<>();
         
+        Queue<Pair> q=new LinkedList<>();
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -33,6 +34,7 @@ class Solution {
         
         int[] dr={-1,0,1,0};
         int[] dc={0,1,0,-1};
+        
         while(!q.isEmpty())
         {
             Pair p=q.poll();
@@ -43,13 +45,14 @@ class Solution {
             
             for(int i=0;i<4;i++)
             {
-            int nr=r+dr[i];
-            int nc=c+dc[i];
-            if(nr>=0 && nc>=0 && nr<m && nc<n && vis[nr][nc]==0)
-            {
-                q.add(new Pair(nr,nc,d+1));
-                vis[nr][nc]=1;
-            }
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                
+                if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==0)
+                {
+                    q.add(new Pair(nr,nc,d+1));
+                    vis[nr][nc]=1;
+                }
             }
         }
         return dis;
