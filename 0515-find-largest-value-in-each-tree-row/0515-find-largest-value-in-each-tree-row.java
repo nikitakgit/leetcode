f0b1d691824a0c896162333ac493f71901ace0d5
nikitakgit <130883808+nikitakgit@users.noreply.github.com>
2024-12-25 21:01:58 +0530

    Time: 2 ms (83.77%), Space: 45.4 MB (64.94%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -15,16 +15,15 @@
  */
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
-        Map <Integer , Integer> map = new HashMap(); 
-       dfs(root, 0, map);
-       return new ArrayList(map.values());
+        Map<Integer,Integer> map=new HashMap<>();
+        dfs(root,0,map);
+        return new ArrayList(map.values());
     }
-
-    private void dfs(TreeNode root, int level, Map<Integer , Integer> map){
+    private void dfs(TreeNode root,int level,Map<Integer,Integer> map)
+    {
         if(root==null) return;
-
-        map.put(level, Math.max(root.val, map.getOrDefault(level, Integer.MIN_VALUE)));
-        dfs(root.left, level+1, map);
-        dfs(root.right, level+1, map);
+        map.put(level,Math.max(root.val,map.getOrDefault(level,Integer.MIN_VALUE)));
+        dfs(root.left,level+1,map);
+        dfs(root.right,level+1,map);
     }
 }
